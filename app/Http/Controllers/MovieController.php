d1cd4293148620f41b9510bc0e11d92e70687c85
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-03-17 19:00:44 +0100

    Connects controller to model +Creates movies route

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all();
+        dd($movies);
+
+        return view('movies', compact('movies'));
+    }
 }
